4f84c5e4eb81d70ca2cbb1936a4322d21e0be9dc
Kr4k3nEU <80364768+KrakenEU@users.noreply.github.com>
2024-02-28 13:38:51 +0100

    Add files via upload

--- penkraken.py
+++ penkraken.py
@@ -6,6 +6,7 @@ import ipaddress
 import nmap
 import signal
 import wafw00f
+import time
 
 # Import modules
 import modules.os_identification as os_identify
@@ -75,8 +76,9 @@ class Welcome:
     def display_welcome_message(self):
         welcome_message = f"{colors['blue']}\n========================= Welcome to ============================={colors['bright_green']}{self.tool_name}{colors['reset']}"
         print(welcome_message)
+        
 
-class Menu:
+class Enumeration:
     def __init__(self):
         self.os = ''
         self.ports = ''
@@ -214,7 +216,7 @@ class Menu:
                     self.cracking()
 
         except:
-            print("Invalid option")
+            print(f"{colors['red']}[-] Invalid option{colors['reset']}")
 
 # Calling Independent Modules
     def os_identification(self):  
@@ -247,6 +249,108 @@ class Menu:
     def cracking(self):
         self.cracked = Cracker.Init()
 
+
+
+def RecoverFile(name):
+    dic = {}
+    with open(name, 'r') as file:
+            file_content = file.read()
+    # Patterns
+    host_pattern = re.compile(r'Host: ([\d.]+)')
+    osint_pattern = re.compile(r'OSINT')
+    metadata_pattern = re.compile(r'Metadata:((?:(?!\n\n).)*)', re.DOTALL)
+    mails_pattern = re.compile(r'Mails:((?:(?!\n\n\n\n).)*)', re.DOTALL)
+    domains_pattern = re.compile(r'Domains:((?:(?!\n\n).)*)', re.DOTALL)
+    usernames_pattern = re.compile(r'Usernames:((?:(?!\n\n).)*)', re.DOTALL)
+    hash_cracking_pattern = re.compile(r'Hash Cracking((?:(?!\n\n).)*)', re.DOTALL)
+    os_pattern = re.compile(r'OS: (.+)')
+    ports_pattern = re.compile(r'Oppened Ports:((?:(?!\n\n).)*)', re.DOTALL)
+    nmap_pattern = re.compile(r'Nmap Scan:((?:(?!\n\n).)*)', re.DOTALL)
+    fuzzing_pattern = re.compile(r'Fuzzing:((?:(?!\n\n).)*)', re.DOTALL)
+    wafs_pattern = re.compile(r'WAFS: (.+)')
+    balancers_pattern = re.compile(r'Load Balancers:((?:(?!\n\n\n\n).)*)', re.DOTALL)
+
+    
+    entries = re.split(r'(?=Host:)', file_content)
+    # Process each entry
+    for entry in entries: 
+
+        # OSINT part
+        osint_match = osint_pattern.search(entry)
+        
+        if osint_match:
+            osint_key = osint_match.group(0).strip()
+            metadata_match = metadata_pattern.search(entry)
+            mails_match = mails_pattern.search(entry)
+            usernames_match = usernames_pattern.search(entry)
+            domains_match = domains_pattern.search(entry)
+            entry_dict = {}
+
+            if metadata_match:
+                entry_dict['Metadata'] = metadata_match.group(1).strip() + '\n'
+
+            if mails_match:
+                entry_dict['Mails'] = mails_match.group(1).strip() + '\n'
+
+            if usernames_match:
+                entry_dict['Usernames'] = usernames_match.group(1).strip() + '\n'
+
+            if domains_match:
+                entry_dict['Domains'] = domains_match.group(1).strip() + '\n'
+
+            dic[osint_key] = entry_dict
+
+        entry_dict = {}
+
+        # Pass Cracking Part
+
+        hash_match = hash_cracking_pattern.search(entry)
+
+        if hash_match:
+            hash_key = hash_match.group(1).strip()
+            hash_val, password = hash_key.split(': ', 1)
+            sub_dict = {hash_val: password+ '\n'}
+            dic['Hash Cracking'] = sub_dict
+
+
+        # Hosts Part
+
+        host_match = host_pattern.search(entry)
+
+        if host_match:
+
+            host_key = host_match.group(0).strip()
+            
+            os_match = os_pattern.search(entry)
+            if os_match:
+                entry_dict['OS'] = os_match.group(1).strip() + '\n'
+
+            ports_match = ports_pattern.search(entry)
+            if ports_match:
+                entry_dict['Oppened Ports'] = ports_match.group(1) + '\n'
+            
+            nmap_match = nmap_pattern.search(entry)
+            if nmap_match:
+                entry_dict['Nmap Scan'] = nmap_match.group(1) + '\n'
+
+            wafs_match = wafs_pattern.search(entry)
+            if wafs_match:
+                entry_dict['WAFS'] = wafs_match.group(1).strip() +'\n'
+            
+            fuzzing_match = fuzzing_pattern.search(entry)
+            if fuzzing_match:
+                entry_dict['Fuzzing'] = fuzzing_match.group(1).strip()
+            
+            balancers_match = balancers_pattern.search(entry)
+            if balancers_match:
+                entry_dict['Load Balancers'] = balancers_match.group(1).strip()
+
+            dic[host_key] = entry_dict
+    
+
+    return dic   
+
+
 if __name__ == "__main__":
     penkraken_welcome = Welcome(f"""
 ==================================================================                      
@@ -262,7 +366,16 @@ ______   ____   ____ |    |/ _|___________  |  | __ ____   ____
     # Display Welcome message
     penkraken_welcome.display_welcome_message()
 
-    # General Variables
+    while True:
+        print(f"{colors['green']}\n[+] OPENING PENKRAKEN PANEL! {colors['reset']}")
+        option = input(f"{colors['bright_green']}\n[1] Recover file from other session to continue\n[2] Jump to modules\n\n{colors['blue']}[>] Choose an option: {colors['reset']}")
+        if option != '1' and option != '2':
+            print(f"{colors['green']}\n[-] Invalid Option! {colors['reset']}")
+        else:
+            break
+
+    # Enumeration Variables
+    Results_dic = {}
     os_name = ''
     ports_discovered = ''
     wafs = ''
@@ -273,48 +386,153 @@ ______   ____   ____ |    |/ _|___________  |  | __ ____   ____
     shells = ''
     osint = ''
     cracked = ''
+
+    # Recover File
+    if option == '1':
+        name = input(f"{colors['blue']}\n[>] Name of the Recoverable File: {colors['reset']}")
+
+        # Read content from the file and update variables
+        Results_dic = RecoverFile(name)
+              
+        print(f"{colors['green']}\n[+] Document Loaded to {colors['red']}PenKraken {colors['green']}!\n[+] Loading Contents...{colors['reset']}")
+        time.sleep(1)
+        process = subprocess.Popen(f'cat {name}',stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, text=True)
+        while True:
+            out = process.stdout.readline()
+            time.sleep(0.025)
+            if out == '' and process.poll() is not None:
+                break
+            else:
+                if out:
+                    print(out.strip())
+        print(f"{colors['green']}\n[+] Done! ")
+    
     # Choose an Option:
     while True:
-        penkraken_menu = Menu()
+        penkraken_enum = Enumeration()
 
-        # Update Variables
+        # Update Enumeration Variables
         try:
-            if penkraken_menu.os != '':
-                os_name = penkraken_menu.os
-            if penkraken_menu.ports != '': 
-                ports_discovered = penkraken_menu.ports
-            if penkraken_menu.wafs != '':
-                wafs = penkraken_menu.wafs
-            if penkraken_menu.balancers != '':
-                balancers = penkraken_menu.balancers
-            if penkraken_menu.discovery != '':
-                discovery = penkraken_menu.discovery
-            if penkraken_menu.fuzzer != '':
-                fuzzer = penkraken_menu.fuzzer
-            if penkraken_menu.exploits != '':
-                exploits = penkraken_menu.exploits
-            if penkraken_menu.shells != '':
-                shells = penkraken_menu.shells
-            if penkraken_menu.osint != '':
-                osint = penkraken_menu.osint
-            if penkraken_menu.cracked != '':
-                cracked = penkraken_menu.cracked
+            if penkraken_enum.os != '':
+                os_name = penkraken_enum.os[1]
+                if f'Host: {penkraken_enum.os[0]}' in Results_dic:
+                    Results_dic[f'Host: {penkraken_enum.os[0]}'].update({'OS' : penkraken_enum.os[1]+'\n'})
+                else:
+                    Results_dic[f'Host: {penkraken_enum.os[0]}'] = {}
+                    Results_dic[f'Host: {penkraken_enum.os[0]}']['OS'] = penkraken_enum.os[1]+'\n'
+
+            if penkraken_enum.ports != '': 
+                ports_discovered = penkraken_enum.ports[1]
+                if f'Host: {penkraken_enum.ports[0]}' in Results_dic:
+                    Results_dic[f'Host: {penkraken_enum.ports[0]}'].update({'Oppened Ports' : penkraken_enum.ports[1]+'\n'})
+                    Results_dic[f'Host: {penkraken_enum.ports[0]}'].update({'Nmap Scan' : penkraken_enum.ports[2]+'\n'})
+
+                else:
+                    Results_dic[f'Host: {penkraken_enum.ports[0]}'] = {}
+                    Results_dic[f'Host: {penkraken_enum.ports[0]}']['Oppened Ports'] = penkraken_enum.ports[1]+'\n'
+                    Results_dic[f'Host: {penkraken_enum.ports[0]}']['Nmap Scan'] = penkraken_enum.ports[2]+'\n'
+
+
+            if penkraken_enum.wafs != '':
+                wafs = penkraken_enum.wafs
+                if f'Host: {penkraken_enum.wafs[0]}' in Results_dic:
+                    Results_dic[f'Host: {penkraken_enum.wafs[0]}'].update({'WAFS' : penkraken_enum.wafs[1].strip()+'\n'})
+                else:
+                    Results_dic[f'Host: {penkraken_enum.wafs[0]}'] = {}
+                    Results_dic[f'Host: {penkraken_enum.wafs[0]}']['WAFS'] = penkraken_enum.wafs[1].strip()+'\n'
+
+            if penkraken_enum.balancers != '':
+                balancers = penkraken_enum.balancers[1]
+                if f'Host: {penkraken_enum.balancers[0]}' in Results_dic:
+                    Results_dic[f'Host: {penkraken_enum.balancers[0]}'].update({'Load Balancers' : penkraken_enum.balancers[1].strip()+'\n\n'})
+                else:
+                    Results_dic[f'Host: {penkraken_enum.balancers[0]}'] = {}
+                    Results_dic[f'Host: {penkraken_enum.balancers[0]}']['Load Balancers'] = penkraken_enum.balancers[1].strip()+'\n\n'
+
+            if penkraken_enum.discovery != '':
+                try:
+                    discovery = penkraken_enum.discovery[1]
+                    if f'Host: {penkraken_enum.discovery[0]}' in Results_dic:
+                        Results_dic[f'Host: {penkraken_enum.balancers[0]}'].update({'Nuclei' : penkraken_enum.balancers[1].strip()+'\n'})
+                    else:
+                        Results_dic[f'Host: {penkraken_enum.discovery[0]}'] = {}
+                        Results_dic[f'Host: {penkraken_enum.balancers[0]}']['Nuclei'] = penkraken_enum.balancers[1].strip()+'\n'
+                except:
+                    discovery = penkraken_enum.discovery
+
+            if penkraken_enum.fuzzer != '':
+                fuzzer = penkraken_enum.fuzzer[1]
+                if f'Host: {penkraken_enum.fuzzer[0]}' in Results_dic:
+                    Results_dic[f'Host: {penkraken_enum.fuzzer[0]}'].update({f'Fuzzing-{penkraken_enum.fuzzer[2]}' :penkraken_enum.fuzzer[1].strip()+'\n'})
+                else:
+                    Results_dic[f'Host: {penkraken_enum.fuzzer[0]}'] = {}
+                    Results_dic[f'Host: {penkraken_enum.fuzzer[0]}'][f'Fuzzing'] = penkraken_enum.fuzzer[1].strip()+'\n'
+
+            if penkraken_enum.exploits != '':
+                exploits = penkraken_enum.exploits
+
+            if penkraken_enum.shells != '':
+                shells = penkraken_enum.shells
+
+            if penkraken_enum.osint != '':
+                osint = penkraken_enum.osint[1]
+                if f'OSINT' in Results_dic:
+                    Results_dic[f'OSINT'].update({penkraken_enum.osint[0] : penkraken_enum.osint[1]})
+                else:
+                    Results_dic[f'OSINT'] = {}
+                    Results_dic[f'OSINT'][penkraken_enum.osint[0]] = penkraken_enum.osint[1]
+
+            if penkraken_enum.cracked != '':
+                cracked = penkraken_enum.cracked
+                hash_val = penkraken_enum.cracked.split(':')[-2].replace(' ','')
+                pattern = re.compile(r'\x1b\[[0-9;]*m')
+                hash_val = re.sub(pattern, '', hash_val)
+                if 'Hash Cracking' in Results_dic:
+                    Results_dic['Hash Cracking'].update({hash_val : penkraken_enum.cracked.split(':')[1].split('\n')[0]+'\n'})
+                else:
+                    Results_dic['Hash Cracking'] = {}
+                    Results_dic['Hash Cracking'][hash_val] = penkraken_enum.cracked.split(':')[-1].split('\n')[0]+'\n'
+
+
 
         except:
             pass
 
-        # Perform more actions
+        # Export Results to a recoverable file
+        #try:
+        # Write New Data
         
+        #except:
+        #    print('Could Not write')
+                    
+
+        print(Results_dic)
+
+        # Perform more actions
+
         while True:
             cont = input(f"{colors['blue']}\n[>] Would you like to perform more actions? (y/n): {colors['reset']}")
             if 'y' in str(cont).lower():
                 break
             elif 'n' in str(cont).lower():
+                input_name = input(f"{colors['blue']}\n[>] Name of the Resulting report file (Default = PenKraken-Report.txt): {colors['reset']}")
+                if input_name != '':
+                    name = input_name
+                else:
+                    name = 'PenKraken-Report.txt'
+                file = open(name, 'w')
+                write = ''
+                for k,v in Results_dic.items():
+                    if k.split()[-1] not in write:
+                        write+=f"{colors['red']}"+k+f"\n{colors['reset']}"
+                    for s,i in v.items():
+                        write+=f"{colors['green']}"+s+f": {colors['magenta']}"+i+f"\n{colors['reset']}"
+
+                file.write(write)
+
                 print(f"{colors['red']}\n[+] Exiting Now...\n[+] Thanks for Using PenKraken!!{colors['reset']}")
                 exit()
             else:
                 print(f"{colors['red']}\n[-] That's not an option, try again{colors['reset']}")
 
-  
 
- 
